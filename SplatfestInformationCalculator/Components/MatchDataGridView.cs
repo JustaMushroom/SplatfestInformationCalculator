c7135995256f8f46fc37aa44d53de3c05cb055eb
JustaMushroom <ramshaw.ryan@outlook.com>
2023-04-02 04:13:01 +0100

    Create custom DataGridView for displaying Matches The new view is not actually used yet

---
+++ SplatfestInformationCalculator/Components/MatchDataGridView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplatfestInformationCalculator.Components
+{
+	public class MatchDataGridView: DataGridView
+	{
+		public MatchDataGridView() : base()
+		{
+			ColumnHeaderMouseDoubleClick += CellHeaderDoubleClick;
+		}
+
+		private void CellHeaderDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+		{
+			Sort(null, System.ComponentModel.ListSortDirection.Ascending);
+		}
+	}
+}
